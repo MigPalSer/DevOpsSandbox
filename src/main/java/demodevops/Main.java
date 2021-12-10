9e3d00a3c72556daa095807767793a62c2b8ea17
Miguel Palomino <mlpv@gft.com>
2021-12-10 08:10:00 +0100

    init

---
+++ src/main/java/demodevops/Main.java
@@ -0,0 +1,9 @@
+package demodevops;
+
+public class Main {
+
+	public static void main(String[] args) {
+System.out.println("vamos a empezar");
+	}
+
+}
---
+++ src/test/java/demodevopstest/TestBase.java
@@ -0,0 +1,19 @@
+package demodevopstest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test2() {
+		assertFalse(false);
+	}
+	
+}
